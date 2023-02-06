584fbf38455870f02f7195c92b829ec45dc304c6
mahagostinho <marcella.c.agostinho@gmail.com>
2023-02-06 20:01:50 -0300

    Acrescentar funcoes de eventos

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: marcarva <marcarva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 17:00:48 by marcarva          #+#    #+#             */
-/*   Updated: 2023/02/05 18:05:50 by marcarva         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:52:33 by marcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,7 @@ int	main(int argc, char **argv)
 
 	check_input(argc, argv[1]);
 	parse_map(argv[1], &game);
+	game_init(&game);
+	mlx_hooks(&game);
+	mlx_loop(game.mlx.mlx_ptr);
 }
