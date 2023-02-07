82429b500b1a818973439ed54f3bf08898d81e05
mahagostinho <marcella.c.agostinho@gmail.com>
2023-02-07 17:20:39 -0300

    atualizar funcoes para eventos de direcionamento do player pelo teclado

--- src/6-hooks.c
+++ src/6-hooks.c
@@ -6,7 +6,7 @@
 /*   By: marcarva <marcarva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 14:57:10 by marcarva          #+#    #+#             */
-/*   Updated: 2023/02/06 19:10:54 by marcarva         ###   ########.fr       */
+/*   Updated: 2023/02/07 15:48:00 by marcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,21 @@ int	handle_keypress(int keysym, t_game *game)
 {
 	if (keysym == XK_Escape)
 		close_game(game);
+	if (keysym == XK_d || keysym == XK_Right)
+		move_player(game, game->player.i, game->player.j + 1);
+	if (keysym == XK_a || keysym == XK_Left)
+		move_player(game, game->player.i, game->player.j - 1);
+	if (keysym == XK_w || keysym == XK_Up)
+		move_player(game, game->player.i - 1, game->player.j);
+	if (keysym == XK_s || keysym == XK_Down)
+		move_player(game, game->player.i + 1, game->player.j);
 	return (0);
 }
 
 void	mlx_hooks(t_game *game)
 {
-	// loop para caso de não ocorrer evento
-	mlx_loop_hook(game->mlx.mlx_ptr, &render_game, game);
-	// evento de teclado
 	mlx_hook(game->mlx.win_ptr, KeyPress, KeyPressMask, &handle_keypress, game);
-	// fechar a janela no x
-	mlx_hook(game->mlx.win_ptr, DestroyNotify, ButtonPressMask, &close_game, game);
+	mlx_hook(game->mlx.win_ptr, DestroyNotify, ButtonPressMask,
+		&close_game, game);
+	mlx_loop_hook(game->mlx.mlx_ptr, &render_game, game);
 }
