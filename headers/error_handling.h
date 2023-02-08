345ce4e2ecae67699ccc05576935415fbb65d9f9
mahagostinho <marcella.c.agostinho@gmail.com>
2023-02-08 18:48:47 -0300

    Refatorar arquivo error_handling.c
    Foi criado um header específico para o .c em questão. Reduzindo a quantidade de includes e definições atribuídos em um único header anteriormente (so_long.h).

--- headers/error_handling.h
+++ headers/error_handling.h
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_handling.c                                   :+:      :+:    :+:   */
+/*   error_handling.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marcarva <marcarva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/04 17:00:27 by marcarva          #+#    #+#             */
-/*   Updated: 2023/02/08 16:05:30 by marcarva         ###   ########.fr       */
+/*   Created: 2023/02/08 16:03:00 by marcarva          #+#    #+#             */
+/*   Updated: 2023/02/08 18:13:35 by marcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../headers/error_handling.h"
+#ifndef ERROR_HANDLING_H
+# define ERROR_HANDLING_H
 
-void	error_message(char *message)
-{
-	ft_printf("Error\n%s\n", message);
-	exit(EXIT_FAILURE);
-}
+# include "so_long.h"
+# include "../libs/libft/headers/libft.h"
+# include "cleaning.h"
 
-void	map_error(char *message, t_game *game)
-{
-	free_map(game);
-	ft_printf("Error\n%s\n", message);
-	exit(EXIT_FAILURE);
-}
+void	error_message(char *message);
+void	map_error(char *message, t_game *game);
+void	game_error(char *message, t_game *game);
 
-void	game_error(char *message, t_game *game)
-{
-	ft_printf("Error\n%s\n", message);
-	free_game(game);
-	exit(EXIT_FAILURE);
-}
+#endif
